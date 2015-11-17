093c4da494c77ef06fe59abc9f78ae2e73b0183e
NataliaLKB <natalialeebaltazar@gmail.com>
2015-11-17 14:34:40 +0000

    changing name of function to something more readable :book:

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 	var keyCombo = [116, 101, 115, 116] // T E S T
     var typedKeys = [];
 
-    function areArraysEqual (a, b) {
+    function arraysAreEqual (a, b) {
 	  	if (a.length != b.length) {
 	  	  	return false;
 	  	}
@@ -21,13 +21,13 @@
 	document.onkeypress = function(e) {
 	    e = e || window.event;
 
-	    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+	    var charCode = (typeof e.which == 'number') ? e.which : e.keyCode;
 
 	    if (charCode === 116 || charCode === 101 || charCode === 115 || charCode === 116) {
 	    	typedKeys.push(charCode);
 
-	    	if (areArraysEqual( typedKeys, keyCombo )) {
-				console.log("Event goes here!");
+	    	if (arraysAreEqual( typedKeys, keyCombo )) {
+				console.log('Event goes here!');
 
 	    	}
 	    }
