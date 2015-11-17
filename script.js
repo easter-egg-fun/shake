91af343a7a223a9655bb31301532a2b7a27f04b5
NataliaLKB <natalialeebaltazar@gmail.com>
2015-11-17 16:07:17 +0000

    trigger event by js so lasting shaking

--- script.js
+++ script.js
@@ -8,13 +8,13 @@
         if (a.length != b.length) {
             return false;
         }
-    
+
         for (var i=0; i<a.length; i++) {
             if (a[i] != b[i]) {
                 return false;
             }
         }
-    
+
         return true;
     }
 
